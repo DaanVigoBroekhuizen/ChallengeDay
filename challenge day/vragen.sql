be5331482491a3b445842af778c194c3f85c738c
daan broekhuizen <daan.broekhuizen@student.rocva.nl>
2020-03-12 11:42:55 +0100

    Add files via upload

---
+++ challenge day/vragen.sql
@@ -0,0 +1,11 @@
+USE vragen;
+
+-- CREATE TABLE `vragen`.`members`(
+-- `id` int(11) NOT NULL AUTO_INCREMENT ,
+-- `username` varchar(30) NOT NULL ,
+-- `password` varchar(128) NOT NULL ,
+-- PRIMARY KEY (`id`) ,
+-- UNIQUE KEY `username` (`username`)
+-- ) ENGINE = MYISAM DEFAULT CHARSET = utf8;
+
+INSERT INTO `members` (`username`, `password`) VALUES (`myuser`, `mypassword`);
---
+++ inlog.php
@@ -0,0 +1,64 @@
+<?PHP 
+
+$host = '127.0.0.1';
+$db   = 'vragen';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+try {
+     $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+
+$username = $_GET['username'];
+$password = $_GET['password'];
+$con=mysqli_connect("mysql","database_user","database_password","vragen");
+ if (mysqli_connect_error())
+ {
+     echo "failed to connect to MySQL: " . mysqli_connect_error();
+ }
+
+ $qz = "SELECT id FROM memebers where username='".$username."' and password='".$password."'";
+ $qz = str_replace("\'", "",$qz);
+ $result = mysqli_query($con,$qz);
+ while($row = mysqli_fetch_array($result))
+ {
+     echo $row['id'];
+ }
+ mysqli_close($con);
+
+?>
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="nav">
+    <ul class="navi">
+        <li class="nav"> <a href="index.php" title="terug">terug</a></li>
+        <li class="nav"> <a href="aanmelden.php" title="naar het aanmeld scherm">aanmelden</a></li>
+    </ul>
+</div>
+
+<h1>welkom bij de inlog pagina</h1>
+    
+<form action="index.php" method="GET" name="form">
+    <input type="text" name="username">
+    <input type="text" name="password">
+    <input type="submit" name="submit">
+</form>
+
+</body>
+</html>
